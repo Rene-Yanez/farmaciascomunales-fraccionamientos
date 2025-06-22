9885c7e59ab4bc702e671be33f01107b79a913fb
Rene-Yanez <rene.yanez2001@alumnos.ubiobio.cl>
2025-06-22 18:48:44 -0400

    fraccionamientos

---
+++ src/main/java/com/farmaciascomunales/fraccionamientos/FraccionamientosApplication.java
@@ -0,0 +1,13 @@
+package com.farmaciascomunales.fraccionamientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraccionamientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraccionamientosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmaciascomunales/fraccionamientos/FraccionamientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmaciascomunales.fraccionamientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraccionamientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
